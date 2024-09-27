0498be454d24e2e2ec9e944bb2ed1bdb69af0da0
HierroPagotto <hierro.reino@gmail.com>
2024-09-27 10:41:37 -0300

    update

--- Aula12/index.php
+++ Aula12/index.php
@@ -12,6 +12,12 @@
             require_once "Ave.php";
             require_once "Peixe.php";
             require_once "Reptil.php";
+            require_once "Canguru.php";
+            require_once "Cachorro.php";
+            require_once "Cobra.php";
+            require_once "Tartaruga.php";
+            require_once "GoldFish.php";
+            require_once "Arara.php";
 
             $a = new Ave();
             $m = new Mamifero();
@@ -27,6 +33,37 @@
             print_r($m);
             print_r($p);
             print_r($r);
+
+            $c[0] = new Canguru();
+            $c[1] = new Cachorro();
+            $c[2] = new Cobra();
+
+            $c[0]->locomover();
+            $c[0]->usarBolsa();
+
+            $c[1]->emitirSom();
+            $c[1]->abanarRabo();
+
+            $c[2]->locomover();
+
+            print_r($c[0]);
+            print_r($c[1]);
+            print_r($c[2]);
+
+            $t = new Tartaruga();
+            $g = new GoldFish();
+            $ar = new Arara();
+
+            $t->locomover();
+
+            $g->locomover();
+            $g->emitirSom();
+            
+            $ar->alimentar();
+
+            print_r($t);
+            print_r($g);
+            print_r($ar);
         ?>
     </pre>
 </body>
